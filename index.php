26bb594b918d082707bb5c449e18ad7bc215a150
Francesco Pio Squarcella-Gorgoglione <127195599+Fra-Sq@users.noreply.github.com>
2026-06-30 17:40:29 +0200

    Add autocomplete attributes to password fields

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ function checklen()
                                             <input type="email" class="form-control" placeholder="Your Email *" name="email"  />
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" id="password" name="password" onkeyup='check();' required/>
+                                            <input type="password" class="form-control" placeholder="Password *" id="password" name="password" autocomplete="new-password" onkeyup='check();' required/>
                                         </div>
                                         
                                         <div class="form-group">
@@ -136,7 +136,7 @@ function checklen()
                                             <input type="tel" minlength="10" maxlength="10" name="contact" class="form-control" placeholder="Your Phone *"  />
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control"  id="cpassword" placeholder="Confirm Password *" name="cpassword"  onkeyup='check();' required/><span id='message'></span>
+                                            <input type="password" class="form-control"  id="cpassword" placeholder="Confirm Password *" name="cpassword" autocomplete="new-password" onkeyup='check();' required/><span id='message'></span>
                                         </div>
                                         <input type="submit" class="btnRegister" name="patsub1" onclick="return checklen();" value="Register"/>
                                     </div>
@@ -157,7 +157,7 @@ function checklen()
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" name="password3" required/>
+                                            <input type="password" class="form-control" placeholder="Password *" name="password3" autocomplete="current-password" required/>
                                         </div>
                                         
                                         <input type="submit" class="btnRegister" name="docsub1" value="Login"/>
@@ -181,7 +181,7 @@ function checklen()
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" name="password2" required/>
+                                            <input type="password" class="form-control" placeholder="Password *" name="password2" autocomplete="current-password" required/>
                                         </div>
                                         
                                         <input type="submit" class="btnRegister" name="adsub" value="Login"/>
@@ -203,5 +203,3 @@ function checklen()
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     </html>
-
-  
